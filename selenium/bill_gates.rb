5faaf6ce02f8faad44613cf4930a63da75a64dfe
thiggy1342 <d.adam.thigpen@gmail.com>
2018-10-10 15:02:25 +0000

    Updating from server

--- selenium/bill_gates.rb
+++ selenium/bill_gates.rb
@@ -1,16 +1,22 @@
 require 'selenium-webdriver'
 
 def bill_gates(message_endpoint)
-  # caps = Selenium::WebDriver::Remote::Capabilities.new
-  # caps["acceptInsecureCerts"] = true
-  # driver = Selenium::WebDriver.for(:remote, url: "http://localhost:4444/wd/hub", desired_capabilities: :chrome)
-  driver = Selenium::WebDriver.for(:chrome, desired_capabilities: :chrome)
-  driver.navigate.to("http://BigBillyMoneyBags:21b341bf77e44006eeaecf446d4646c546d399792951b989a572ee6dc3240333@0.0.0.0:15123#{message_endpoint}")
-  driver.manage.add_cookie(name: "flag", value: "DefinitelyNotFreddieVonChimpenheimer")
-  sleep 1
-  driver.find_element(id:"message_content").send_keys get_message
-  driver.find_element(xpath: "//input[@value='Create Message']").click
-  driver.close
+  puts "Beginning Selenium browser session."
+  begin
+    #caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "--no-sandbox"] })
+    #caps["nogui"] = true
+    #caps["acceptInsecureCerts"] = true
+    driver = Selenium::WebDriver.for(:remote, url: "http://localhost:4444/wd/hub", desired_capabilities: :chrome)
+    #driver = Selenium::WebDriver.for(:chrome, driver_path: "/usr/local/share/chromedriver", desired_capabilities: caps)
+    driver.navigate.to("http://BigBillyMoneyBags:21b341bf77e44006eeaecf446d4646c546d399792951b989a572ee6dc3240333@192.168.0.1:15123#{message_endpoint}")
+    driver.manage.add_cookie(name: "flag", value: "DefinitelyNotFreddieVonChimpenheimer")
+    sleep 1
+    driver.find_element(id:"message_content").send_keys get_message
+    driver.find_element(xpath: "//input[@value='Create Message']").click
+    driver.close
+  rescue Exception => e
+    puts "Unable to run Selenium Commands. Encountered Error: #{e.message}\n"	
+  end
 end
 
 def get_message
@@ -28,7 +34,9 @@ def get_message
   messages[rand(messages.length-1)]
 end
 
-Dir["cache/messages/*.txt"].each do |filename|
+puts "Start of script run"
+
+Dir[File.expand_path(File.dirname(__FILE__))+"/../cache/messages/*.txt"].each do |filename|
   Thread.new {
     f = File.open(filename, "r")
     bill_gates(f.read)
@@ -36,3 +44,6 @@ Dir["cache/messages/*.txt"].each do |filename|
     File.delete(filename)
   }.join
 end
+
+puts "end of script run"
+
